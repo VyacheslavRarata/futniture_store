4d9aaaeeeabb06b9125a241c62f5e085b0f54d76
Vyacheslav Rarata <cayneir@gmail.com>
2015-11-16 17:29:05 +0200

    - slider carousel;

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items : 1,
+		singleItem:true,
+		navigation : true,
+		//autoPlay:true,
+		pagination : true,
+		//autoHeight:true
+	});
+
+});
